1df4aa4d80f22b18fa8e0e83ec4a39399e27a78f
tinh.lm <luongmihtinh>
2024-06-05 00:53:24 +0700

    card + title

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import { Header } from 'antd/es/layout/layout'
+import { Layout, Card, Col, Row, Tooltip, Button, Space } from 'antd'
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+
+// assets
+import logoTrello from './assets/logo-trello.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout>
+        <Header
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#1677ff',
+            height: '40px'
+          }}
+        >
+          <img src={logoTrello} alt='logo trello' height={80} width={80}></img>
+        </Header>
+      </Layout>
+      <main>
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card 
+              title="Card title" 
+              bordered={false} 
+              extra={
+                <Space gutter={2}>
+                  <Tooltip title="add">
+                    <Button shape="circle" icon={<PlusOutlined />} />
+                  </Tooltip>
+
+                  <Tooltip title="delete">
+                    <Button shape="circle" icon={<DeleteOutlined />} />
+                  </Tooltip>
+                </Space>
+              }>
+              Card content
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+        </Row>
+      </main>
     </>
   )
 }
